a8465064048b26a5c32ad824ce9f5a95033df5b9
ChiaraInMiniatura <chiarag295@gmail.com>
2022-04-25 17:20:20 +0200

    aggiunta funzione genra bombe ma non funziona

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ document.querySelector("#btn_azzera").addEventListener("click", azzera);
 init();
 Livello();
 azzera();
-generaBombe();
 
 
 
@@ -38,11 +37,9 @@ function init(){
     // mettere i numeri nei quadretti
     sq.innerHTML = `<span> ${i} </span>`;
 
-    bomba = generaBombe (Livello);
-    console.log("bombe", bomba);
-
-  
   }
+  
+  
 }
 
 
@@ -93,37 +90,23 @@ function numRandom(min, max) {
 }
 
 // funzione genera bombe
-function generaBombe(Livello ){
+function generaBombe(min , max ){
+  
   let arrayBombe = [];
 
-  while(arrayBombe.length <16){
-      bomba = numRandom(min, max);
-      if(!arrayBombe.includes(bomba)){
-          arrayBombe.push(bomba);
-      }
+  while(arrayBombe.length < 16){
+
+    bomba = numRandom(1, Livello);
+
+    if(!arrayBombe.includes(bomba)){
+
+      arrayBombe.push(bomba);
+
+    }
+
   }
+
+  console.log(arrayBombe);
   return arrayBombe;
 }
-console.log(arrayBombe)
-
-
-// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l’inizializzazione di git).
-// ****L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-// con difficoltà 1 => tra 1 e 100
-// con difficoltà 2 => tra 1 e 81
-// con difficoltà 3 => tra 1 e 49
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-// I numeri nella lista delle bombe non possono essere duplicati.
-// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
-// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-// **BONUS:**
-// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-// ****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-// **Consigli del giorno:** :party_wizard:
-// ****Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
